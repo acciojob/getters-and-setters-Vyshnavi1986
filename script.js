d28c7a8d88d5d8f116b7fda3bae62e0f1ce26300
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-22 17:39:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Person {}
+class Person 
+{
+	Person(String name,Number age)
+	{
+		name=this.name;
+		age=this.age;
+	}
+	get name()
+	{
+		console.log(this.name);
+	}
+	set name(Number age)
+	{
+		console.log(this.age);
+	}
+	
+}
 
-class Student extends Person {}
+class Student extends Person 
+{
+	study(String name)
+	{
+		console.log(name+"is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person 
+{
+	teach(String name)
+	{
+		console.log(name+"is teaching")
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
